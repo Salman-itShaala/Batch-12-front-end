ad6e2e15b933d95b59e296bab29b77f79bf489cf
Salman-itShaala <salman.shaikh@ithsaala.com>
2024-12-07 17:12:42 +0530

    added code from js day - 06

---
+++ javascript/day -06/script.js
@@ -0,0 +1,24 @@
+// 1. Easy -->
+// Write a function that takes two numbers as input and returns their sum.
+
+// Create a program that asks the user to enter their name using prompt, and then displays a personalized greeting using alert.
+
+// Write a program to check whether a number entered by the user is even or odd.
+
+// Write a function that takes a string as input and returns its length.
+
+// Create a program that asks the user to enter a number and prints the multiplication table for that number up to 10.
+
+// Write a program to calculate the sum of all numbers between 1 and 100.
+
+// Write a program to find the largest of three numbers entered by the user.
+
+// Write a program that generates a random number between 1 and 10
+
+//  and allows the user to guess it. Show appropriate messages for higher, lower, or correct guesses.
+
+// functions in
+
+// created a function
+
+alert("Hii how are you");
